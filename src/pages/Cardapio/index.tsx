235737f19e88a79e726ba63c1e1d23dba801966e
Mauricio-MdS <mauricio.martins.silva@gmail.com>
2022-04-05 21:57:45 -0300

    Ordenador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,9 +3,11 @@ import { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio(){
     const [busca, setBusca] = useState('');
+    const [filtro, setFiltro] = useState<number | null>(null);
     return(
         <main>
             <nav className={styles.menu}>
@@ -23,7 +25,8 @@ export default function Cardapio(){
                     setBusca={setBusca}
                 />
                 <div className={styles.cardapio__filtros}>
-                    <Filtros/>
+                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                    <Ordenador/>
                 </div>
             </section>
         </main>
